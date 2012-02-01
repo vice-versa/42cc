a5a609cff3826d65eade4b3a82052b28a790cd53
Panov Sergey <spanovaleks@gmail.com>
2012-02-01 20:02:44 +0600

    ticket:1 add more tests

--- panov/models.py
+++ panov/models.py
@@ -7,7 +7,8 @@ class Person(models.Model):
     name = models.CharField(verbose_name=u'Name', max_length=255)
     last_name = models.CharField(verbose_name=u'Last Name', max_length=255)
     
-    birthdate = models.DateField(verbose_name=u'Date of birth')
+    birthdate = models.DateField(verbose_name=u'Date of birth',
+                                 null=True, blank=True)
     
     bio = models.TextField(verbose_name=u'BIO', max_length=255,
                            null=True, blank=True)
@@ -23,9 +24,11 @@ class ContactInfo(models.Model):
     email = models.EmailField(verbose_name=u'Email',
                               null=True, blank=True)
     
-    jabber = models.CharField(verbose_name=u'JabberID', max_length=255)
+    jabber = models.CharField(verbose_name=u'JabberID', max_length=255,
+                              null=True, blank=True)
     
-    skype = models.CharField(verbose_name=u'SkypeID', max_length=255)
+    skype = models.CharField(verbose_name=u'SkypeID', max_length=255,
+                             null=True, blank=True)
     
     other_contacts = models.TextField(verbose_name=u'Other Contacts', max_length=255,
                            null=True, blank=True)
--- panov/tests.py
+++ panov/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from tddspry.django.cases import DatabaseTestCase, HttpTestCase
+from panov.models import Person, ContactInfo
 
 class AdminUserTest(DatabaseTestCase):
     
@@ -26,4 +27,40 @@ class TestMainPage(HttpTestCase):
     def test_main_page(self):
         self.go200('index')
 
+class PersonTest(DatabaseTestCase):
+    
+    def setUp(self):
+        
+        self.person = Person.objects.create(name=u'name',
+                                           last_name='last_name',
+                                           )
+        
+        
+    def tearDown(self):
+        self.person.delete()
+        
+    
+    def test_unicode(self):
+        
+        person = self.person
+        self.assertEqual(unicode(self.person), u' '.join([person.name, person.last_name]))
+        
+class ContactInfoTest(DatabaseTestCase):
+    
+    def setUp(self):
+        
+        self.person = Person.objects.create(name=u'name',
+                                           last_name='last_name',
+                                           )
+        self.ci = ContactInfo.objects.create(owner=self.person)
+        
+    def tearDown(self):
+        self.person.delete()
+
+
+    def test_unicode(self):
+        
+        person = self.ci.owner
+        right_value = u' '.join([person.name, person.last_name, u'contacts'] )
+        self.assertEqual(unicode(self.ci), right_value)
     
