73f24a0c12d3524e7dd8c610c577ab5eb295261c
Panov Sergey <spanovaleks@gmail.com>
2012-02-02 11:45:46 +0600

    ticket:1 views pep8

--- panov/views.py
+++ panov/views.py
@@ -2,14 +2,15 @@
 from django.shortcuts import render
 from panov.models import Person
 
+
 def index(request):
-    
+
     person = Person.objects.get(name=u'Sergey', last_name=u'Panov')
-    
+
     context = {
-               'person':person,
+               'person': person,
                'ci': person.contactinfo,
                }
-    
-    
+
+
     return render(request, 'index.html', context)
