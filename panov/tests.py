122b136f0659b2371c82bcc953806c43f52d44a6
Panov Sergey <spanovaleks@gmail.com>
2012-02-08 08:41:26 +0600

    ticket:9 tests

--- panov/tests.py
+++ panov/tests.py
@@ -5,6 +5,10 @@ from request.models import Request
 from django.conf import settings
 from django.core.management import call_command
 from django.db import models
+from django.core import urlresolvers
+from django.contrib.contenttypes.models import ContentType
+import subprocess
+import shlex
 
 
 class AdminUserTest(HttpTestCase):
@@ -35,6 +39,18 @@ class MainPageTest(HttpTestCase):
         self.find(str(person.contactinfo.email))
         self.find(str(person.contactinfo.jabber))
         self.find(str(person.contactinfo.skype))
+
+    def test_admin_edit_url(self):
+        person = Person.objects.latest('id')
+        obj = person
+        self.go200('index')
+        content_type = ContentType.objects.get_for_model(obj.__class__)
+        admin_url = urlresolvers.reverse("admin:%s_%s_change" % \
+                               (content_type.app_label,
+                                content_type.model),
+                                     args=(obj.id,))
+
+        self.find(admin_url)
 
 
 class RequestTest(HttpTestCase):
@@ -111,6 +127,19 @@ class SettingsProcessorTest(HttpTestCase):
 
 
 class ModelsCommandTest(DatabaseTestCase):
+
+    def setUp(self):
+        self.out_file = "tests_out.txt"
+        self.error_file = "tests_error.txt"
+
+    def tearDown(self):
+        command = 'rm -f %s' % self.out_file
+        args = shlex.split(command)
+        subprocess.Popen(args)
+
+        command = 'rm -f %s' % self.error_file
+        args = shlex.split(command)
+        subprocess.Popen(args)
 
     def test_command(self):
         from panov import models as p_models
